f37d4815fac487452f4f593b854ded357876294d
박현우 <simuoo96@gmail.com>
2025-12-07 00:07:50 +0900

    feat : TS로 리팩토링

--- src/pages/WorkoutList.tsx
+++ src/pages/WorkoutList.tsx
@@ -1,25 +1,39 @@
+// src/pages/WorkoutList.tsx
 import React, { useEffect, useState } from "react";
 import WorkoutCard from "../components/WorkoutCard";
 import { db } from "../firebase/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { isFavorite, toggleFavorite } from "../utils/localStorage";
-import Ad from "../components/Ad.tsx";
+import Ad from "../components/Ad";
 
-export default function WorkoutList() {
-  const [workouts, setWorkouts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [_, force] = React.useReducer((x) => x + 1, 0);
+// Firestore에서 가져오는 데이터 타입
+interface Workout {
+  id: string;
+  // 나머지 필드는 자유롭게 들어갈 수 있도록
+  [key: string]: any;
+}
 
+const WorkoutList = () => {
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [, force] = React.useReducer((x: number) => x + 1, 0);
+
+
+  // 불러오기 부분
   useEffect(() => {
+    // FireStore에서 비동기로 가져오기
     const fetchWorkouts = async () => {
       const snap = await getDocs(collection(db, "workouts"));
-      const list = snap.docs.map((doc) => ({
+
+      const list: Workout[] = snap.docs.map((doc) => ({
         id: doc.id,
-        ...doc.data(),
+        ...(doc.data() as Omit<Workout, "id">),
       }));
+
       setWorkouts(list);
       setLoading(false);
     };
+
     fetchWorkouts();
   }, []);
 
@@ -27,7 +41,7 @@ export default function WorkoutList() {
 
   return (
     <div>
-      <Ad/>
+      <Ad />
       <h1>운동 목록</h1>
       {workouts.map((w) => (
         <WorkoutCard
@@ -36,10 +50,12 @@ export default function WorkoutList() {
           favorite={isFavorite(w.id)}
           onToggleFavorite={() => {
             toggleFavorite(w.id);
-            force();
+            force(); // 즐겨찾기 상태 다시 렌더링
           }}
         />
       ))}
     </div>
   );
-}
+};
+
+export default WorkoutList;
